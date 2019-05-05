5ef4901883e9209bb32719463fbca5a310bda9b4
victor <victormorais08@hotmail.com>
2019-05-05 17:21:27 -0300

    primeiro commit

---
+++ src/trabalhotheobaldo/Alunos.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhotheobaldo;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+/**
+ *
+ * @author victo
+ */
+public class Alunos {
+    
+    private String nome;
+
+    public static final String PROP_NOME = "nome";
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        String oldNome = this.nome;
+        this.nome = nome;
+        propertyChangeSupport.firePropertyChange(PROP_NOME, oldNome, nome);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+       
+    }
+    
+        private String email;
+
+    public static final String PROP_EMAIL = "email";
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        String oldEmail = this.email;
+        this.email = email;
+        propertyChangeSupport.firePropertyChange(PROP_EMAIL, oldEmail, email);
+    }
+
+    private String cidade;
+
+    public static final String PROP_CIDADE = "cidade";
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        String oldCidade = this.cidade;
+        this.cidade = cidade;
+        propertyChangeSupport.firePropertyChange(PROP_CIDADE, oldCidade, cidade);
+    }
+
+    private String uf;
+
+    public static final String PROP_UF = "uf";
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        String oldUf = this.uf;
+        this.uf = uf;
+        propertyChangeSupport.firePropertyChange(PROP_UF, oldUf, uf);
+    }
+
+    private int codigo;
+
+    public static final String PROP_CODIGO = "codigo";
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        int oldCodigo = this.codigo;
+        this.codigo = codigo;
+        propertyChangeSupport.firePropertyChange(PROP_CODIGO, oldCodigo, codigo);
+    }
+
+
+}
---
+++ src/trabalhotheobaldo/Main.java
@@ -0,0 +1,13 @@
+
+package trabalhotheobaldo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        CadastroCliente c =new CadastroCliente();
+        c.setVisible(true); 
+     
+    }
+    
+}
